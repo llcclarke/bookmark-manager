4fa29323a90f0a7a961b23baec30a59df751e609
Emmett Walsh <emmett.finarr.walsh@gmail.com>
2016-05-05 15:50:06 +0100

    Added tag filter

--- app/app.rb
+++ app/app.rb
@@ -17,6 +17,7 @@ class BookmarkManager < Sinatra::Base
   get '/links/new' do
     erb :'links/new'
   end
+
   post '/links' do
     link = Link.new(url: params[:url], title: params[:title])
     tag = Tag.create(name: params[:tags])
@@ -24,6 +25,13 @@ class BookmarkManager < Sinatra::Base
     link.save
     redirect to('/links')
   end
+
+  get '/tags/:name' do
+    tag = Tag.first(name: params[:name])
+    @links = tag ? tag.links : []
+    erb :'links/index'
+  end
+
     run! if app_file == $0
 
 end
