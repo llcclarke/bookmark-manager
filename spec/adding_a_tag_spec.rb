c3c26c7a4d7f4215ff3fa47c4c07b253cc12de1b
Emmett Walsh <emmett.finarr.walsh@gmail.com>
2016-05-05 16:21:48 +0100

    Failing multiple tag tests

--- spec/adding_a_tag_spec.rb
+++ spec/adding_a_tag_spec.rb
@@ -8,4 +8,14 @@ feature "Adding a tag" do
     link = Link.first
     expect(link.tags.map(&:name)).to include("dope")
   end
+
+  scenario "Add multiple tags when you save a link" do
+    visit 'links/new'
+    fill_in "url", with: "http://www.zombo.com/"
+    fill_in "title", with: "This is Zombocom"
+    fill_in "tags", with: "dope rope pope"
+    click_button "Create link"
+    link = Link.first
+    expect(link.tags.map(&:name)).to include("dope", "pope", "rope")
+  end
 end
